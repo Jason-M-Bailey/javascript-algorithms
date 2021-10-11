15ef26c760f5a48bd4b84180e173ea9ccd72cf49
Jason Bailey <jmbailey816@gmail.com>
2021-10-11 19:50:37 -0400

    Reverse a string

---
+++ 003-Reverse-a-String/Solved/index.js
@@ -0,0 +1,13 @@
+// Reverse a StringPassed
+// Reverse the provided string.
+
+function reverseString(str) {
+  // split the string by ""
+  // reverse the string
+  // join the string by ""
+
+  return (newString = str.split("").reverse().join(""));
+}
+
+// run 'node index.js' in your terminal
+console.log(reverseString("hello"));
---
+++ 003-Reverse-a-String/Unsolved/index.js
@@ -0,0 +1,9 @@
+// Reverse a StringPassed
+// Reverse the provided string.
+
+function reverseString(str) {
+  // your code here
+}
+
+// run 'node index.js' in your terminal
+console.log(reverseString("hello"));
