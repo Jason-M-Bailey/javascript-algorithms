403dc26db0e3e21ee8b58c81d4fe5dc7a00b595b
Jason Bailey <jmbailey816@gmail.com>
2021-10-12 19:45:27 -0400

    Factorial, zeroes and ones, common element

---
+++ 011-Factorial/Unsolved/factorial.js
@@ -0,0 +1,7 @@
+// Write code to create a function that returns the factorial of `num`
+
+// If given `0`, the factorial should be `1`. Otherwise, assume that the given number will be a whole positive number.
+
+var factorial = function (num) {
+  // your code here
+};
