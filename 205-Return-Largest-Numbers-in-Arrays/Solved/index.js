c8b7cfb527592f543272809f9803dd767836de5a
Jason Bailey <jmbailey816@gmail.com>
2021-10-13 15:45:00 -0400

    Largest numbers in array SOLVED!

--- 205-Return-Largest-Numbers-in-Arrays/Solved/index.js
+++ 205-Return-Largest-Numbers-in-Arrays/Solved/index.js
@@ -2,21 +2,42 @@
 // Return an array consisting of the largest number from each provided sub-array.
 
 function largestOfFour(arr) {
-    // your code here
-    
+  let answer = [];
 
-
-
+  for (let i = 0; i < arr.length; i++) {
+    answer.push(Math.max(...arr[i]));
   }
-  
-  // run 'node index.js' in your terminal
-  // result should return [5, 27, 39, 1001]
-  console.log(
-    largestOfFour([
-      [4, 5, 1, 3],
-      [13, 27, 18, 26],
-      [32, 35, 37, 39],
-      [1000, 1001, 857, 1],
-    ])
-  );
-  
+
+  return answer;
+}
+
+// run 'node index.js' in your terminal
+// result should return [5, 27, 39, 1001]
+console.log(
+  largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
+  ])
+);
+
+// should return [9, 35, 97, 1000000].
+console.log(
+  largestOfFour([
+    [4, 9, 1, 3],
+    [13, 35, 18, 26],
+    [32, 35, 97, 39],
+    [1000000, 1001, 857, 1],
+  ])
+);
+
+// should return [25, 48, 21, -3]
+console.log(
+  largestOfFour([
+    [17, 23, 25, 12],
+    [25, 7, 34, 48],
+    [4, -10, 18, 21],
+    [-72, -3, -17, -10],
+  ])
+);
