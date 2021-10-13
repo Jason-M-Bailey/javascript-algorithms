d31c258e42de5cb765f42e00a4c97c0572fefb18
Jason Bailey <jmbailey816@gmail.com>
2021-10-12 20:49:17 -0400

    Additional examples for reverse words

--- 208-Reverse-Words/Solved/index.js
+++ 208-Reverse-Words/Solved/index.js
@@ -4,13 +4,20 @@
 // "This is an example!" ==> "sihT si na !elpmaxe"
 // "double  spaces"      ==> "elbuod  secaps"
 
-function reverseWords(string) {
-  var reversed = string.split("").reverse().join("");
-
-  return reversed;
+function reverseWords(str) {
+  return str
+    .split(" ")
+    .map(function (word) {
+      return word.split("").reverse().join("");
+    })
+    .join(" ");
 }
 
 // run 'node index.js' in your terminal
 console.log(`${reverseWords("This is an example!")} === sihT si na !elpmaxe`);
 
 console.log(`${reverseWords("double  spaces")} === elbuod  secaps`);
+
+console.log(`${reverseWords("a b c d")} === a b c d`);
+
+console.log(`${reverseWords("double  spaced  words")} === elbuod  decaps  sdrow`);
