3ab4230828e421a9caf17a6780833dd75ce80c62
Jason Bailey <jmbailey816@gmail.com>
2021-10-12 21:53:34 -0400

    Square every digit SOLVED!

---
+++ 212-Square-Every-Digit/Solved/index.js
@@ -0,0 +1,19 @@
+// Square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+// Note: The function accepts an integer and returns an integer
+
+function squareDigits(num) {
+  var string = num.toString();
+  var results = [];
+  for (var i = 0; i < string.length; i++) {
+    results[i] = string[i] * string[i];
+  }
+  return Number(results.join(""));
+}
+
+// run 'node index.js' in your terminal
+console.log(`811181 === ${squareDigits("9119")}`);
+console.log(`1616 === ${squareDigits("44")}`);
+console.log(`496481 === ${squareDigits("789")}`);
