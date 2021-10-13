7016c844612691ca07b602d32ee9563268eb8ae1
Jason Bailey <jmbailey816@gmail.com>
2021-10-12 20:06:38 -0400

    Product of largest two, character count, title case

---
+++ 012-Title-Case/Unsolved/title-case.js
@@ -0,0 +1,5 @@
+// Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
+
+// https://www.tabnine.com/academy/javascript/how-to-use-the-touppercase-method/
+
+var titleCase = function (str) {};
