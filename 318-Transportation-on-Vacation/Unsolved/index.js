70e3d794dc5b9a41f68ccd606d89a7a958975a26
Jason Bailey <jmbailey816@gmail.com>
2021-10-24 09:38:33 -0400

    Transportation on Vacation SOLVED

---
+++ 318-Transportation-on-Vacation/Unsolved/index.js
@@ -0,0 +1,22 @@
+// Transportation on Vacation
+
+// After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+// You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+// Write a code that gives out the total amount for different days(d).
+
+function rentalCarCost(d) {
+  // your code here
+}
+
+console.log(`40 === ${rentalCarCost(1)}`);
+console.log(`80 === ${rentalCarCost(2)}`);
+console.log(`100 === ${rentalCarCost(3)}`);
+console.log(`140 === ${rentalCarCost(4)}`);
+console.log(`180 === ${rentalCarCost(5)}`);
+console.log(`220 === ${rentalCarCost(6)}`);
+console.log(`230 === ${rentalCarCost(7)}`);
+console.log(`270 === ${rentalCarCost(8)}`);
